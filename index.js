22609356159f4fa55f63ee03de2b0ab57441cd24
jcuevas <gcq012@gmail.com>
2018-05-03 00:21:15 -0400

    config file added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const graphqlHTTP = require('express-graphql')
 const chalk = require('chalk')
 const schema = require('./schema/schema')
 const mongoose = require('mongoose')
+const { db } = require('./config')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -13,7 +14,7 @@ mongoose.connection.once('open', () => {
     console.log('app connected')
 })
 
-mongoose.connect('mongodb://jcq012:123456@ds263619.mlab.com:63619/foredev')
+mongoose.connect(`mongodb://${db.username}:${db.passport}@ds263619.mlab.com:63619/${db.name}`)
 
 app.use('/graphql', graphqlHTTP({
     schema,
